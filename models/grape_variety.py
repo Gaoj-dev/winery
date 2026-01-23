26aba97640894b590aa377d7e97766aae524f8d8
Gao <lidernetgaojie@gmail.com>
2026-01-23 18:04:22 +0100

    Final de plot, falta testear añadiendo resto

---
+++ models/grape_variety.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class uva(models.Model):
+    _name = 'bodega.uva'
+    _description = 'Tipo de uva'
+
+    name = fields.Char(string="Nombre", required=True)
+    color = fields.Selection(
+        [
+            ('tinto', 'Tinto'), 
+            ('blanco', 'Blanco'), 
+            ('rosado', 'Rosado'),
+        ],
+        string="Clasificacion",
+        required=True
+    )
+    origin_region_id = fields.Many2one("bodega.origen", string="Region")
+    is_seedless = fields.Boolean(string="Sin pepitas")
+    acidity_level = fields.Float(string="Acidez")
+    notes = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
--- models/plot.py
+++ models/plot.py
@@ -3,21 +3,21 @@
 from odoo import models, fields, api
 
 
-class winery(models.Model):
+class plot(models.Model):
     _name = 'winery.plot'
-    _description = 'winery.plot'
+    _description = 'Parcela'
     #Identificacion
-    id = fields.Integer(string="Numero de parcela", required=True)
+    plot_number = fields.Integer(string="Numero de parcela", required=True)
     name = fields.Char(string="Nombre", required=True, readonly=True, compute="_compute_name")
     alias = fields.Char(string="Alias")
     cadastral_reference = fields.Char(string="Referencia catastral")#TODO posible metodo verificar si es valido
     #Localizacion
-    country_id = fields.Many2one('res.country', string="País", default=68, compute="_onchange_country_id")
-    state_id = fields.Many2one('res.country.state', string="Provincia", default=427)
+    country_id = fields.Many2one('res.country', string="País", default=68)
+    state_id = fields.Many2one('res.country.state', string="Provincia", compute="_onchange_country_id")
     locality = fields.Char(string="Localidad")
     surface_ref = fields.Float(string="Superficie en hectáreas")
-    #Datos vitícoras
-    grape_variety = fields.Many2Many(string="Variedad/es de la uva")
+    #Datos vitícolas
+    grape_variety_id = fields.Many2one(comodel_name="winery.grape_variety", string="Variedad de la uva")#Si es many2many name puede fallar
     aggregation = fields.Char()
     zone = fields.Char()
 
@@ -39,23 +39,23 @@ class winery(models.Model):
 
     description = fields.Text(string="Descripcion")
 
-@api.depends('plot_number', 'state_id', 'aggregation', 'grape_variety_id')
-def _compute_name(self):
-    for rec in self:
-        parts = [
-            f"Nº {rec.plot_number}" if rec.plot_number else None,
-            rec.state_id.name if rec.state_id else None,
-            rec.aggregation,
-            rec.grape_variety_id,
-        ]
-        rec.name = " - ".join(filter(None, parts))
+    @api.depends('plot_number', 'state_id', 'aggregation', 'grape_variety_id')
+    def _compute_name(self):
+        for rec in self:
+            parts = [
+                f"Nº {rec.plot_number}" if rec.plot_number else None,
+                rec.state_id.name if rec.state_id else None,
+                rec.aggregation,
+                rec.grape_variety_id,
+            ]
+            rec.name = " - ".join(filter(None, parts))
 
-@api.onchange('country_id')
-def _onchange_country_id(self):
-    if self.state_id and self.state_id.country_id != self.country_id:
-        self.state_id = False
-    return {
-        'domain': {
-            'state_id': [('country_id', '=', self.country_id.id)]
+    @api.onchange('country_id')
+    def _onchange_country_id(self):
+        if self.state_id and self.state_id.country_id != self.country_id:
+            self.state_id = False
+        return {
+            'domain': {
+                'state_id': [('country_id', '=', self.country_id.id)]
+            }
         }
-    }
